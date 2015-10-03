83c6887bdc91d4d581f233f8772607b89c3b9d30
EvgeniyaS <e.shekhova@gmail.com>
2015-10-03 13:37:56 +0300

    Homework 18
    Contact Creation, addressbook-native-tests

---
+++ addressbook-native-tests/src/com/example/fw/HelperBase.java
@@ -0,0 +1,12 @@
+package com.example.fw;
+
+
+public abstract class HelperBase {
+	
+protected final ApplicationManager manager;
+
+public HelperBase(ApplicationManager applicationManager) {
+	this.manager = applicationManager;
+}
+	
+}
